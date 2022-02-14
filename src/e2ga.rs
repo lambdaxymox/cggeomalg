ee0798aebeb0bc13f47b8ad480b1b092b645a92e
LambdaXymox <lambda.xymox@gmail.com>
2022-02-13 22:59:14 -0600

    Add indexing for two-dimensional multivectors.

--- src/e2ga.rs
+++ src/e2ga.rs
@@ -3,10 +3,13 @@ use crate::scalar::{
     ScalarSigned,
     ScalarFloat,
 };
+use std::ops::{
+    Index,
+    IndexMut,
+};
 
-const BASIS_COUNT: usize = 4;
-
-pub enum Basis {
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+pub enum BasisElement {
     /// The `1` , or scalar component of a Euclidean multivector.
     C,
     E1,
@@ -15,7 +18,7 @@ pub enum Basis {
 }
 
 pub struct EuclideanMultivector2<S> {
-    data: [S; BASIS_COUNT],
+    data: [S; 4],
 }
 
 impl<S> EuclideanMultivector2<S> 
@@ -35,5 +38,63 @@ where
             data: [S::zero(), S::zero(), S::zero(), S::zero()],
         }
     }
+
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.data.len()
+    }
 }
 
+impl<S> Index<usize> for EuclideanMultivector2<S> 
+where
+    S: Scalar
+{
+    type Output = S;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.data[index]
+    }
+}
+
+impl<S> IndexMut<usize> for EuclideanMultivector2<S> 
+where
+    S: Scalar
+{
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.data[index]
+    }
+}
+
+impl<S> Index<BasisElement> for EuclideanMultivector2<S> 
+where
+    S: Scalar
+{
+    type Output = S;
+
+    fn index(&self, index: BasisElement) -> &Self::Output {
+        let idx = match index {
+            BasisElement::C => 0,
+            BasisElement::E1 => 1,
+            BasisElement::E2 => 2,
+            BasisElement::E12 => 3,
+        };
+
+        &self.data[idx]
+    }
+}
+
+impl<S> IndexMut<BasisElement> for EuclideanMultivector2<S>
+where
+    S: Scalar
+{
+    fn index_mut(&mut self, index: BasisElement) -> &mut Self::Output {
+        let idx = match index {
+            BasisElement::C => 0,
+            BasisElement::E1 => 1,
+            BasisElement::E2 => 2,
+            BasisElement::E12 => 3,
+        };
+
+        &mut self.data[idx]
+    }
+}
