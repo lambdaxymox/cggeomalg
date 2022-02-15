7175b85afbafd13c072db183a71ef902bbc9d443
LambdaXymox <lambda.xymox@gmail.com>
2022-02-14 20:54:07 -0600

    Add left contraction (inner product) for two-dimensional multivectors.

--- src/e2ga.rs
+++ src/e2ga.rs
@@ -7,6 +7,7 @@ use std::ops::{
     Index,
     IndexMut,
     BitXor,
+    BitOr,
     Mul,
     Not,
 };
@@ -362,6 +363,74 @@ where
         let result_e1  = a[0] * b[1] + a[1] * b[0];
         let result_e2  = a[0] * b[2] + a[2] * b[0];
         let result_e12 = a[0] * b[3] + a[1] * b[2] - a[2] * b[1] + a[3] * b[0];
+        
+        EuclideanMultivector2::new(result_1, result_e1, result_e2, result_e12)
+    }
+}
+
+impl<S> BitOr<EuclideanMultivector2<S>> for EuclideanMultivector2<S> 
+where
+    S: Scalar
+{
+    type Output = EuclideanMultivector2<S>;
+
+    fn bitor(self, b: EuclideanMultivector2<S>) -> Self::Output {
+        let a = self;
+        let result_1   = a[0] * b[0] + a[1] * b[1] + a[2] * b[2] - a[3] * b[3];
+        let result_e1  = a[0] * b[1] + a[1] * b[0] - a[2] * b[3] + a[3] * b[2];
+        let result_e2  = a[0] * b[2] + a[1] * b[3] + a[2] * b[0] - a[3] * b[1];
+        let result_e12 = a[0] * b[3] + a[3] * b[0];
+        
+        EuclideanMultivector2::new(result_1, result_e1, result_e2, result_e12)
+    }
+}
+
+impl<S> BitOr<&EuclideanMultivector2<S>> for EuclideanMultivector2<S> 
+where
+    S: Scalar
+{
+    type Output = EuclideanMultivector2<S>;
+
+    fn bitor(self, b: &EuclideanMultivector2<S>) -> Self::Output {
+        let a = self;
+        let result_1   = a[0] * b[0] + a[1] * b[1] + a[2] * b[2] - a[3] * b[3];
+        let result_e1  = a[0] * b[1] + a[1] * b[0] - a[2] * b[3] + a[3] * b[2];
+        let result_e2  = a[0] * b[2] + a[1] * b[3] + a[2] * b[0] - a[3] * b[1];
+        let result_e12 = a[0] * b[3] + a[3] * b[0];
+        
+        EuclideanMultivector2::new(result_1, result_e1, result_e2, result_e12)
+    }
+}
+
+impl<S> BitOr<EuclideanMultivector2<S>> for &EuclideanMultivector2<S> 
+where
+    S: Scalar
+{
+    type Output = EuclideanMultivector2<S>;
+
+    fn bitor(self, b: EuclideanMultivector2<S>) -> Self::Output {
+        let a = self;
+        let result_1   = a[0] * b[0] + a[1] * b[1] + a[2] * b[2] - a[3] * b[3];
+        let result_e1  = a[0] * b[1] + a[1] * b[0] - a[2] * b[3] + a[3] * b[2];
+        let result_e2  = a[0] * b[2] + a[1] * b[3] + a[2] * b[0] - a[3] * b[1];
+        let result_e12 = a[0] * b[3] + a[3] * b[0];
+        
+        EuclideanMultivector2::new(result_1, result_e1, result_e2, result_e12)
+    }
+}
+
+impl<'a, 'b, S> BitOr<&'b EuclideanMultivector2<S>> for &'a EuclideanMultivector2<S> 
+where
+    S: Scalar
+{
+    type Output = EuclideanMultivector2<S>;
+
+    fn bitor(self, b: &'b EuclideanMultivector2<S>) -> Self::Output {
+        let a = self;
+        let result_1   = a[0] * b[0] + a[1] * b[1] + a[2] * b[2] - a[3] * b[3];
+        let result_e1  = a[0] * b[1] + a[1] * b[0] - a[2] * b[3] + a[3] * b[2];
+        let result_e2  = a[0] * b[2] + a[1] * b[3] + a[2] * b[0] - a[3] * b[1];
+        let result_e12 = a[0] * b[3] + a[3] * b[0];
         
         EuclideanMultivector2::new(result_1, result_e1, result_e2, result_e12)
     }
