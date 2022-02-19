ddf240bf46723cc99b922ae516766f97accd162c
LambdaXymox <lambda.xymox@gmail.com>
2022-02-19 16:07:10 -0600

    Correct unit test.

--- tests/test_e2ga.rs
+++ tests/test_e2ga.rs
@@ -13,7 +13,7 @@ mod e2ga_test {
     fn test_multivector_addition() {
         let mv1: EuclideanMultivector2<isize> = EuclideanMultivector2::new(1, 2, 3, 4);
         let mv2: EuclideanMultivector2<isize> = EuclideanMultivector2::new(5, 6, 7, 8);
-        let expected = EuclideanMultivector2::new(6, 6, 10, 12);
+        let expected = EuclideanMultivector2::new(6, 8, 10, 12);
         let result = mv1 + mv2;
 
         assert_eq!(result, expected);
