13f12debca4c22cecb4643827070423de98de2c4
LambdaXymox <lambda.xymox@gmail.com>
2022-02-19 16:06:18 -0600

    Add unit test for addition.

--- tests/test_e2ga.rs
+++ tests/test_e2ga.rs
@@ -1,2 +1,24 @@
 extern crate cggeomalg;
 extern crate num_traits;
+
+
+#[cfg(test)]
+mod e2ga_test {
+    use cggeomalg::e2ga::{
+        EuclideanMultivector2,
+    };
+
+
+    #[test]
+    fn test_multivector_addition() {
+        let mv1: EuclideanMultivector2<isize> = EuclideanMultivector2::new(1, 2, 3, 4);
+        let mv2: EuclideanMultivector2<isize> = EuclideanMultivector2::new(5, 6, 7, 8);
+        let expected = EuclideanMultivector2::new(6, 6, 10, 12);
+        let result = mv1 + mv2;
+
+        assert_eq!(result, expected);
+    }
+
+
+}
+
