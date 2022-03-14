2770f49b40195c48f5b7266df27624c340b45bda
LambdaXymox <lambda.xymox@gmail.com>
2022-03-13 22:52:44 -0500

    Implement outer product.

--- src/e3ga.rs
+++ src/e3ga.rs
@@ -1043,86 +1043,119 @@ where
         )
     }
 }
-/*
-impl<S> ops::BitXor<EuclideanMultivector2<S>> for EuclideanMultivector2<S> 
+
+impl<S> ops::BitXor<EuclideanMultivector3<S>> for EuclideanMultivector3<S> 
 where
     S: Scalar
 {
-    type Output = EuclideanMultivector2<S>;
+    type Output = EuclideanMultivector3<S>;
 
     #[inline]
-    fn bitxor(self, other: EuclideanMultivector2<S>) -> Self::Output {
+    fn bitxor(self, other: EuclideanMultivector3<S>) -> Self::Output {
         let a = self;
         let b = other;
-        let result_1   = a[0] * b[0];
-        let result_e1  = a[0] * b[1] + a[1] * b[0];
-        let result_e2  = a[0] * b[2] + a[2] * b[0];
-        let result_e12 = a[0] * b[3] + a[1] * b[2] - a[2] * b[1] + a[3] * b[0];
+        let result_1    = a[0] * b[0];
+        let result_e1   = a[0] * b[1] + a[1] * b[0];
+        let result_e2   = a[0] * b[2] + a[2] * b[0];
+        let result_e3   = a[0] * b[3] + a[3] * b[0];
+        let result_e12  = a[0] * b[4] + a[1] * b[2] - a[2] * b[1] + a[4] * b[0];
+        let result_e23  = a[0] * b[5] + a[2] * b[3] - a[3] * b[2] + a[5] * b[0];
+        let result_e31  = a[0] * b[6] - a[1] * b[3] + a[3] * b[1] + a[6] * b[0];
+        let result_e123 = a[0] * b[7] + a[1] * b[5] + a[2] * b[6] + a[3] * b[4] + a[4] * b[3] + a[5] * b[1] + a[6] * b[2] + a[7] * b[0];
         
-        EuclideanMultivector2::new(result_1, result_e1, result_e2, result_e12)
+        EuclideanMultivector3::new(
+            result_1, 
+            result_e1, result_e2, result_e3, 
+            result_e12, result_e23, result_e31, 
+            result_e123
+        )
     }
 }
 
-impl<S> ops::BitXor<&EuclideanMultivector2<S>> for EuclideanMultivector2<S> 
+impl<S> ops::BitXor<&EuclideanMultivector3<S>> for EuclideanMultivector3<S> 
 where
     S: Scalar
 {
-    type Output = EuclideanMultivector2<S>;
+    type Output = EuclideanMultivector3<S>;
 
     #[inline]
-    fn bitxor(self, other: &EuclideanMultivector2<S>) -> Self::Output {
+    fn bitxor(self, other: &EuclideanMultivector3<S>) -> Self::Output {
         let a = self;
         let b = other;
-        let result_1   = a[0] * b[0];
-        let result_e1  = a[0] * b[1] + a[1] * b[0];
-        let result_e2  = a[0] * b[2] + a[2] * b[0];
-        let result_e12 = a[0] * b[3] + a[1] * b[2] - a[2] * b[1] + a[3] * b[0];
+        let result_1    = a[0] * b[0];
+        let result_e1   = a[0] * b[1] + a[1] * b[0];
+        let result_e2   = a[0] * b[2] + a[2] * b[0];
+        let result_e3   = a[0] * b[3] + a[3] * b[0];
+        let result_e12  = a[0] * b[4] + a[1] * b[2] - a[2] * b[1] + a[4] * b[0];
+        let result_e23  = a[0] * b[5] + a[2] * b[3] - a[3] * b[2] + a[5] * b[0];
+        let result_e31  = a[0] * b[6] - a[1] * b[3] + a[3] * b[1] + a[6] * b[0];
+        let result_e123 = a[0] * b[7] + a[1] * b[5] + a[2] * b[6] + a[3] * b[4] + a[4] * b[3] + a[5] * b[1] + a[6] * b[2] + a[7] * b[0];
         
-        EuclideanMultivector2::new(result_1, result_e1, result_e2, result_e12)
+        EuclideanMultivector3::new(
+            result_1, 
+            result_e1, result_e2, result_e3, 
+            result_e12, result_e23, result_e31, 
+            result_e123
+        )
     }
 }
 
-impl<S> ops::BitXor<EuclideanMultivector2<S>> for &EuclideanMultivector2<S> 
+impl<S> ops::BitXor<EuclideanMultivector3<S>> for &EuclideanMultivector3<S> 
 where
     S: Scalar
 {
-    type Output = EuclideanMultivector2<S>;
+    type Output = EuclideanMultivector3<S>;
 
     #[inline]
-    fn bitxor(self, other: EuclideanMultivector2<S>) -> Self::Output {
+    fn bitxor(self, other: EuclideanMultivector3<S>) -> Self::Output {
         let a = self;
         let b = other;
-        let result_1   = a[0] * b[0];
-        let result_e1  = a[0] * b[1] + a[1] * b[0];
-        let result_e2  = a[0] * b[2] + a[2] * b[0];
-        let result_e12 = a[0] * b[3] + a[1] * b[2] - a[2] * b[1] + a[3] * b[0];
+        let result_1    = a[0] * b[0];
+        let result_e1   = a[0] * b[1] + a[1] * b[0];
+        let result_e2   = a[0] * b[2] + a[2] * b[0];
+        let result_e3   = a[0] * b[3] + a[3] * b[0];
+        let result_e12  = a[0] * b[4] + a[1] * b[2] - a[2] * b[1] + a[4] * b[0];
+        let result_e23  = a[0] * b[5] + a[2] * b[3] - a[3] * b[2] + a[5] * b[0];
+        let result_e31  = a[0] * b[6] - a[1] * b[3] + a[3] * b[1] + a[6] * b[0];
+        let result_e123 = a[0] * b[7] + a[1] * b[5] + a[2] * b[6] + a[3] * b[4] + a[4] * b[3] + a[5] * b[1] + a[6] * b[2] + a[7] * b[0];
         
-        EuclideanMultivector2::new(result_1, result_e1, result_e2, result_e12)
+        EuclideanMultivector3::new(
+            result_1, 
+            result_e1, result_e2, result_e3, 
+            result_e12, result_e23, result_e31, 
+            result_e123
+        )
     }
 }
 
-impl<'a, 'b, S> ops::BitXor<&'b EuclideanMultivector2<S>> for &'a EuclideanMultivector2<S> 
+impl<'a, 'b, S> ops::BitXor<&'b EuclideanMultivector3<S>> for &'a EuclideanMultivector3<S> 
 where
     S: Scalar
 {
-    type Output = EuclideanMultivector2<S>;
+    type Output = EuclideanMultivector3<S>;
 
     #[inline]
     fn bitxor(self, other: &'b EuclideanMultivector3<S>) -> Self::Output {
         let a = self;
         let b = other;
-        let result_1   = a[0] * b[0];
-        let result_e1  = a[0] * b[1] + a[1] * b[0];
-        let result_e2  = a[0] * b[2] + a[2] * b[0];
-        let result_e3  = a[0] * b[3] + a[3] * b[0];
-        let result_e12 = a[0] * b[4] + a[1] * b[2] - a[2] * b[1] + a[4] * b[0];
-        let result_e23 = a[0] * b[5] + a[1] * b[3] * b[3] - a[3] * b[1] + a[5] * b[0];
-        let result_e31 = 
+        let result_1    = a[0] * b[0];
+        let result_e1   = a[0] * b[1] + a[1] * b[0];
+        let result_e2   = a[0] * b[2] + a[2] * b[0];
+        let result_e3   = a[0] * b[3] + a[3] * b[0];
+        let result_e12  = a[0] * b[4] + a[1] * b[2] - a[2] * b[1] + a[4] * b[0];
+        let result_e23  = a[0] * b[5] + a[2] * b[3] - a[3] * b[2] + a[5] * b[0];
+        let result_e31  = a[0] * b[6] - a[1] * b[3] + a[3] * b[1] + a[6] * b[0];
+        let result_e123 = a[0] * b[7] + a[1] * b[5] + a[2] * b[6] + a[3] * b[4] + a[4] * b[3] + a[5] * b[1] + a[6] * b[2] + a[7] * b[0];
         
-        EuclideanMultivector3::new(result_1, result_e1, result_e2, result_e12)
-    
+        EuclideanMultivector3::new(
+            result_1, 
+            result_e1, result_e2, result_e3, 
+            result_e12, result_e23, result_e31, 
+            result_e123
+        )
+    }
 }
-*/
+
 impl<S> ops::BitXor<S> for EuclideanMultivector3<S>
 where
     S: Scalar
