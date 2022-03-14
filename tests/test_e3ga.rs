10e660cfeb5ec0081d6e74ff3b38c2e4954e05b6
LambdaXymox <lambda.xymox@gmail.com>
2022-03-13 23:28:31 -0500

    Define testing module.

---
+++ tests/test_e3ga.rs
@@ -0,0 +1,17 @@
+extern crate cggeomalg;
+extern crate approx;
+extern crate num_traits;
+
+
+#[cfg(test)]
+mod e3ga_tests {
+    use cggeomalg::e2ga::{
+        EuclideanMultivector2,
+    };
+    use approx::{
+        assert_relative_eq,
+    };
+
+
+
+}
