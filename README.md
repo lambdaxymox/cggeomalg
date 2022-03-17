3c276a3da56d229cd6ca36e7a271be68137d72a4
LambdaXymox <lambda.xymox@gmail.com>
2022-03-16 20:21:45 -0500

    Fill in README.md.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Computer Graphics Geometric Algebra Library
+
+## Introduction
+**cggeomalg** is a low-dimensional geometric algebra aimed at 
+real-time computer graphics, real-time physics, and simulations 
+in the language of real-valued Clifford algebras.
+
+## Getting Started
+To use the library in your project, add **cggeomalg** as a dependency in your 
+`Cargo.toml` file:
+```
+[dependencies]
+cggeomalg = "0.2.0"
+```
+After that, place the crate declaration in either your `lib.rs` file or 
+your `main.rs` file
+```rust
+extern crate cggeomalg;
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+/*!
+# Computer Graphics Geometric Algebra Library
+
+## Introduction
+**cggeomalg** is a low-dimensional geometric algebra aimed at 
+real-time computer graphics, real-time physics, and simulations 
+in the language of real-valued Clifford algebras.
+
+## Getting Started
+To use the library in your project, add **cggeomalg** as a dependency in your 
+`Cargo.toml` file:
+```ignore
+[dependencies]
+cggeomalg = "0.2.0"
+```
+After that, place the crate declaration in either your `lib.rs` file or 
+your `main.rs` file
+```rust
+extern crate cggeomalg;
+```
+
+*/
+
 #![no_std]
 extern crate core;
 
